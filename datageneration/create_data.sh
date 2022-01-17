2caf61327ade676f278ebcc56b268b9606f818de
LWagner <st148345@stud.uni-stuttgart.de>
2022-01-17 10:34:59 +0100

    added plotter script

--- datageneration/create_data.sh
+++ datageneration/create_data.sh
@@ -4,4 +4,6 @@ echo "Creating Quantitative Data..."
 Rscript ./quant_faker.R $@
 echo "Creating Qualitative Data..."
 python ./quality_faker.py $@
+echo "Printing..."
+python ./plotter.py
 echo "Done."
